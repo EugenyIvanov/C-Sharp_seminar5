7a4367a8c55fad8596bc4e3df43b1d800129325b
Евгений <ivanychcos@gmail.com>
2022-09-10 15:15:21 +0300

    Add example 031

---
+++ Example031/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.Write("Введите размерность массива: ");
+int size = int.Parse(Console.ReadLine());
+int [] array = GetArray(size, -9, 9);
+
+Console.WriteLine(String.Join(" ", array));
+
+
+int [] GetArray(int size, int min, int max)
+{
+   int [] result = new int[size];
+   for (int i = 0; i < size; i++)
+   {
+    result[i] = new Random().Next(min, max + 1);
+   }
+   return result;
+}
+
+int sumPositive = 0;
+int sumNegative = 0;
+
+foreach(int element in array)
+{
+   sumPositive += element > 0 ? element : 0;
+   sumNegative += element < 0 ? element : 0;
+}
+
+Console.WriteLine($"sum positive: {sumPositive} | sum negative: {sumNegative}");
