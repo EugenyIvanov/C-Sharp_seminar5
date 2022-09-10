ebbaaecba0ae949292f61430fd1eb47ee388f5f4
Евгений <ivanychcos@gmail.com>
2022-09-10 23:00:20 +0300

    Add example 035

---
+++ Example035/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+int size = 123;
+int [] array = GetArray(size, -9, 9);
+
+Console.WriteLine(String.Join(" ", array));
+
+
+int [] GetArray(int size, int min, int max)
+{
+   int [] result = new int[size];
+   for (int i = 0; i < size; i++)
+   {
+    result[i] = new Random().Next(min, max + 1);
+   }
+   return result;
+}
+for(int j = 10; j < 100; j++)
+{
+    Console.Write(array[j] + " ");
+}
