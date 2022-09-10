98b93cfa39e44381fd9a2a237dfabce9e7c75a50
Евгений <ivanychcos@gmail.com>
2022-09-10 23:40:21 +0300

    Add example 037

---
+++ Example037/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+int size = int.Parse(Console.ReadLine());
+int [] array = GetArray(size, 1, 9);
+
+Console.WriteLine(String.Join(" ", array));
+
+int [] GetArray(int size, int min, int max)
+{
+   int [] result = new int[size];
+   for (int i = 0; i < size; i++)
+   {
+    result[i] = new Random().Next(min, max + 1);
+   }
+   return result;
+}
+int j = array.Length -1;
+for (int x = 0; x < j; x++)
+{
+    int product = array[x] * array[j];
+    j--;
+    Console.Write(product + " ");
+}
+if(array.Length % 2 != 0)
+{
+    Console.Write(array[array.Length/2]);
+}
